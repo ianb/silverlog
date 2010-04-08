ae18b615188131f8b2aebe4a4ce973fd2ec9dc60
Ian Bicking <ianb@colorstudy.com>
2010-04-08 17:28:24 -0500

    Moved to a jQuery/Sammy/PURE system instead of rendering server-side templates

--- silverlog/static/script.js
+++ silverlog/static/script.js
@@ -1,6 +1,61 @@
-$(function () {
-  $('.toggle').click(function () {
-    var el = $($(this).attr('toggle-el'));
-    el.slideToggle();
+var app = $.sammy(function () {
+  this.element_selector = '#body';
+
+  this.get('#/', function (context) {
+    moveScreen('#main');
+    $('#main #log-list li:nth-child(1n+2)').remove();
+    $('#header #title-slot').text('index');
+    $.ajax({
+      url: "/api/list-logs",
+      dataType: "json",
+      success: function (result) {
+        $('#main').render(
+          result,
+          {
+            "#log-list li": {
+              "log<-logs": {
+                "a": "log.description",
+                "a@href": function (ctx) {
+                  return '#/view/'+ctx.item.id;
+                }
+              }
+            }
+          });
+      }
+    });
   });
+
+  this.get('#/view/:log_id', function (context) {
+    moveScreen('#log-view');
+    $.ajax({
+      url: "/api/log/"+this.params.log_id,
+      dataType: "json",
+      success: function (result) {
+        $('#header #title-slot').text(result.description);
+        $('#log-view').render(
+          result,
+          {
+            "pre.content": "content"
+          });
+      }
+    });
+  });
+
 });
+
+$(function () {
+  app.run('#/');
+});
+
+var currentScreen = null;
+
+function moveScreen(selector) {
+  if (currentScreen) {
+    $(currentScreen).hide();
+    currentScreen = null;
+  }
+  var el = $(selector);
+  currentScreen = selector;
+  el.show();
+  return el;
+}
